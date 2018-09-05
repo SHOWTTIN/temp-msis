05159cf14e4f3f66494e6feab043b46b7e180775
Xueting Wang <xuetwang@iu.edu>
2018-09-05 13:21:56 -0400

    revise app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,27 @@
 var app = new Vue({
   el: '#dashboardapp',
   data: {
-    message: 'Hello Vue!'
+    {
+    "name" : "Tapestry",
+    "short_description": "Build a visualization layer for the project dashboard",
+    "start_date" : "2018-07-01",
+    "target_date" : "2018-11-03",
+    "budget" : "4950000",
+    "spent" : "3456700",
+    "projected_spend": "4740500",
+    "weekly_effort_target": 400
   }
+},
+computed: {
+  days_left: function(){
+    // this .target_date
+    return 31;
+  }
+},
+methods:{
+  pretty_date: function (d){
+    //do magic
+    return d;
+  }
+}
 })
