df76b82b9fc9755bb088db35387a10e35edde601
Xueting Wang <xuetwang@iu.edu>
2018-09-26 11:05:36 -0400

    inclass-create Team.php

---
+++ app/models/Team.php
@@ -0,0 +1,40 @@
+<?php
+class Team{
+  public $id;
+  public $name;
+
+  public function __construct ($data){
+    $this->id = intval($data['id']);
+    $this->name = $data['name']);
+  }
+
+  public static function findAll(){
+    //1. connect to a database;
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    //2. run a query;
+    $sql = 'SELECT * FROM Team';
+
+    $statement = $db->prepare($sql);
+
+    //3. run the results;
+    $success = $statement -> executed(
+
+    );
+
+    //4. handle the results;
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+
+      //4a. for each row, make a new work object;
+      $theTeam = new Work($row);
+
+      array_push($arr,$theTeam);
+    }
+    //4b. return the array of work objects;
+
+    return $arr;
+  }
+  }
+}
+?>
